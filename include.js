dd80d0be919fe624caadf8a56b5d99e67a56a566
adabru <b.brunnmeier@gmx.de>
2020-01-09 13:53:04 +0100

    new page: current

--- include.js
+++ include.js
@@ -2,12 +2,12 @@
 
 fs = require('fs')
 
-var targets = fs.readdirSync('public').filter(f => f.endsWith('.html')).map(f => ({file:f, content:fs.readFileSync('public/'+f, 'utf-8')}))
+var targets = fs.readdirSync('docs').filter(f => f.endsWith('.html')).map(f => ({file:f, content:fs.readFileSync('docs/'+f, 'utf-8')}))
 var includes = {}
 for(f of fs.readdirSync('html_include'))
   includes[f.substr(0,f.length-5)] = fs.readFileSync('html_include/'+f, 'utf-8')
 for(t of targets) {
   for(i in includes)
     t.content = t.content.replace(new RegExp(`<!--${i}-->[^]*?<!---->`, "g"), `<!--${i}-->\n${includes[i]}`)
-  fs.writeFileSync('public/'+t.file, t.content)
+  fs.writeFileSync('docs/'+t.file, t.content)
 }
